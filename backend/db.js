ed3da568c707977210a547ededaeb1f6b34d6cb9
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2024-11-04 03:21:56 +0100

    Your commit message here3

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 import mongoose from 'mongoose';
 
-const connectDB = async () => {
+export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
