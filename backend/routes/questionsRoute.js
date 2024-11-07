3d7e39cccab8edebc07f8d0c8d1bf85da2612a1b
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2024-11-07 13:40:41 +0100

    Server clean up 2

--- backend/routes/questionsRoute.js
+++
@@ -1,8 +0,0 @@
-// backend/routes/questionsRoute.js
-import express from 'express';
-import { getQuestions } from '../controllers/questionsController.js';
-
-const router = express.Router();
-router.post('/', getQuestions);
-
-export default router;
