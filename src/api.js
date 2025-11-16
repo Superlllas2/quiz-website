345ad05b63c7082e44c24ce7c659da62d8c182a4
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2025-11-16 13:59:33 +0100

    Dev mode added

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: "https://quest-nest-backend-a0171c77d032.herokuapp.com/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     }
